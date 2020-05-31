998b8c4cff45480bd6194e575fd5de8bab048cec
Kessir Adjaho <m.kessir.adjaho@gmail.com>
2020-05-31 15:30:54 +0200

    deploy

---
+++ precache-manifest.f4b14ed1225c19dda1ddddc450a960bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4043d29e7c41d821c7a1",
+    "url": "/taxcalculatorgh/css/app.a31749b8.css"
+  },
+  {
+    "revision": "4043d29e7c41d821c7a1",
+    "url": "/taxcalculatorgh/js/app.1ab5246d.js"
+  },
+  {
+    "revision": "6ff7708f46d08c74fa7f",
+    "url": "/taxcalculatorgh/js/chunk-vendors.6419df19.js"
+  },
+  {
+    "revision": "43121b1e609a822df6807730b5796084",
+    "url": "/taxcalculatorgh/index.html"
+  },
+  {
+    "revision": "5fe14a1f3eed6efcb895eb0aa0de2557",
+    "url": "/taxcalculatorgh/css/spectre.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taxcalculatorgh/robots.txt"
+  }
+];
